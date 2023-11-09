f207a46430703ebba802a0a634fd26d7fa9942d8
morispolanco <morispolanco@gmail.com>
2023-11-09 01:30:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -15,39 +15,39 @@ if LOGGED_IN == True:
 
 
 
-# Título de la aplicación
-st.title("LeybotGt")
-st.markdown("Esta aplicación responde preguntas relacionadas con la legislación de Guatemala.")
-st.text("Por Moris Polanco")
-
-# Campo de entrada para la pregunta o caso
-pregunta = st.text_area("Pregunta o caso")
-
-# Botón para obtener la respuesta
-if st.button("Obtener Respuesta"):
-    if not pregunta:
-        st.warning("Por favor, escriba una pregunta o caso.")
-    else:
-        # Realizar la solicitud a la API de Respell.ai
-        response = requests.post(
-            "https://api.respell.ai/v1/run",
-            headers={
-                "Authorization": "Bearer 260cee54-6d54-48ba-92e8-bf641b5f4805",
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            data=json.dumps({
-                "spellId": "k0GhQkJOn7IKEY-BdghY6",
-                "inputs": {
-                    "pregunta": pregunta
-                }
-            })
-        )
-        
-        # Procesar la respuesta de la API
-        if response.status_code == 200:
-            respuesta = response.json().get("outputs", {}).get("respuesta", "No se pudo obtener una respuesta")
-            st.write("Respuesta:", respuesta)
-        else:
-            st.error("Error al enviar la solicitud a la API")
-                                      
+  # Título de la aplicación
+  st.title("LeybotGt")
+  st.markdown("Esta aplicación responde preguntas relacionadas con la legislación de Guatemala.")
+  st.text("Por Moris Polanco")
+  
+  # Campo de entrada para la pregunta o caso
+  pregunta = st.text_area("Pregunta o caso")
+  
+  # Botón para obtener la respuesta
+  if st.button("Obtener Respuesta"):
+      if not pregunta:
+          st.warning("Por favor, escriba una pregunta o caso.")
+      else:
+          # Realizar la solicitud a la API de Respell.ai
+          response = requests.post(
+              "https://api.respell.ai/v1/run",
+              headers={
+                  "Authorization": "Bearer 260cee54-6d54-48ba-92e8-bf641b5f4805",
+                  "Accept": "application/json",
+                  "Content-Type": "application/json"
+              },
+              data=json.dumps({
+                  "spellId": "k0GhQkJOn7IKEY-BdghY6",
+                  "inputs": {
+                      "pregunta": pregunta
+                  }
+              })
+          )
+          
+          # Procesar la respuesta de la API
+          if response.status_code == 200:
+              respuesta = response.json().get("outputs", {}).get("respuesta", "No se pudo obtener una respuesta")
+              st.write("Respuesta:", respuesta)
+          else:
+              st.error("Error al enviar la solicitud a la API")
+                                        
